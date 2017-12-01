650fdf0fe6f557cd9cf94403705b02dc05ef87a0
SooLee <duplexa@gmail.com>
2017-12-01 16:46:50 -0500

    added testthat

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(Rpairix)
+
+test_check("Rpairix")
---
+++ tests/testthat/test-px_query.R
@@ -0,0 +1,8 @@
+context("test-px_query.R")
+
+test_that("px_query works for query chrX|chrX on 4dn pairs file", {
+  x1 = px_query("inst/test_4dn.pairs.gz","chrX|chrX", linecount.only=TRUE)
+  x2 = system("gunzip -c inst/test_4dn.pairs.gz | awk '$2==\"chrX\" && $4==\"chrX\"' |wc -l", intern=TRUE)
+  expect_equal(x1, x2)
+})
+
