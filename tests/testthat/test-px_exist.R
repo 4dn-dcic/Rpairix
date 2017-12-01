fe75cec3907c026d115eaabbe219c0a8a60f94db
SooLee <duplexa@gmail.com>
2017-12-01 17:04:45 -0500

    updated px_exists test

--- tests/testthat/test-px_exist.R
+++
@@ -1,6 +0,0 @@
-context("test-px_exist.R")
-
-test_that("px_exist works", {
-  expect_equal(px_exist("inst/test_4dn.pairs.gz", "chr1"), TRUE)
-  expect_equal(px_exist("inst/test_4dn.pairs.gz", "2"), FALSE)
-})
---
+++ tests/testthat/test-px_exists.R
@@ -0,0 +1,7 @@
+context("test-px_exists.R")
+
+test_that("px_exists works", {
+  expect_equal(px_exists("inst/test_4dn.pairs.gz", "chr1|chr1"), TRUE)
+  expect_equal(px_exists("inst/test_4dn.pairs.gz", "chr1"), TRUE)
+  expect_equal(px_exists("inst/test_4dn.pairs.gz", "2|4"), FALSE)
+})
