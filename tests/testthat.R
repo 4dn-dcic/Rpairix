819afdf987034bdaef49911982fb64905d23ffd7
SooLee <duplexa@gmail.com>
2017-12-05 11:22:14 -0500

    removed sudo false in travis

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,5 +1,4 @@
 library(testthat)
 library(Rpairix)
 
-Sys.setenv("R_TESTS" = "")
 test_check("Rpairix")
--- tests/testthat/test-px_exists.R
+++ tests/testthat/test-px_exists.R
@@ -4,12 +4,9 @@ test_that("px_exists works", {
   filename1 = system.file(".","test_4dn.pairs.gz",package="Rpairix")
   filename2 = system.file(".","SRR1171591.variants.snp.vqsr.p.vcf.gz",package="Rpairix")
 
-  print(filename1)
-  print(filename2)
-
-  expect_equal(px_exists(filename1, "chr1|chr1"), TRUE)
+  expect_true(px_exists(filename1, "chr1|chr1"))
   expect_equal(px_exists(filename1, "chr1"), FALSE)
   expect_equal(px_exists(filename1, "2|4"), FALSE)
-  expect_equal(px_exists(filename2, "X"), TRUE)
+  expect_true(px_exists(filename2, "X"))
   expect_equal(px_exists(filename2, "Z"), FALSE)
 })
