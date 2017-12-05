12da10661d1142fa601a118a020ce171d7c56273
SooLee <duplexa@gmail.com>
2017-12-05 11:41:43 -0500

    fixed one test for px_exists

--- tests/testthat/test-px_exists.R
+++ tests/testthat/test-px_exists.R
@@ -7,6 +7,6 @@ test_that("px_exists works", {
   expect_true(px_exists(filename1, "chr1|chr1"))
   expect_equal(px_exists(filename1, "chr1"), FALSE)
   expect_equal(px_exists(filename1, "2|4"), FALSE)
-  expect_true(px_exists(filename2, "X"))
+  expect_true(px_exists(filename2, "chrX"))
   expect_equal(px_exists(filename2, "Z"), FALSE)
 })
