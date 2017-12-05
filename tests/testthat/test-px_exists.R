a8c2c2d0aace23d101b7f1b0791f3ad69a40ab27
SooLee <duplexa@gmail.com>
2017-12-05 10:22:35 -0500

    system file name change for tests

--- tests/testthat/test-px_exists.R
+++ tests/testthat/test-px_exists.R
@@ -1,9 +1,12 @@
 context("test-px_exists.R")
 
 test_that("px_exists works", {
-  expect_equal(px_exists("inst/test_4dn.pairs.gz", "chr1|chr1"), TRUE)
-  expect_equal(px_exists("inst/test_4dn.pairs.gz", "chr1"), FALSE)
-  expect_equal(px_exists("inst/test_4dn.pairs.gz", "2|4"), FALSE)
-  expect_equal(px_exists("SRR1171591.variants.snp.vqsr.p.vcf.gz", "X"), TRUE)
-  expect_equal(px_exists("SRR1171591.variants.snp.vqsr.p.vcf.gz", "Z"), FALSE)
+  filename1 = system.file(".","test_4dn.pairs.gz",package="Rpairix")
+  filename2 = system.file(".","SRR1171591.variants.snp.vqsr.p.vcf.gz",package="Rpairix")
+
+  expect_equal(px_exists(filename1, "chr1|chr1"), TRUE)
+  expect_equal(px_exists(filename1, "chr1"), FALSE)
+  expect_equal(px_exists(filename1, "2|4"), FALSE)
+  expect_equal(px_exists(filename2, "X"), TRUE)
+  expect_equal(px_exists(filename2, "Z"), FALSE)
 })
