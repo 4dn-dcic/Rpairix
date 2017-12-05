fa0883b35c0cabc709448c7d02113c17f991bfe1
SooLee <duplexa@gmail.com>
2017-12-05 10:20:40 -0500

    .

--- tests/testthat/test-px_query.R
+++ tests/testthat/test-px_query.R
@@ -1,9 +1,17 @@
 context("test-px_query.R")
 
 test_that("px_query works for query chrX|chrX on 4dn pairs file", {
-  x1 = px_query("inst/test_4dn.pairs.gz","chrX|chrX", linecount.only=TRUE)
-  x2 = system("gunzip -c inst/test_4dn.pairs.gz | awk '$2==\"chrX\" && $4==\"chrX\"' |wc -l", intern=TRUE)
+  filename = system.file(".","test_4dn.pairs.gz",package="Rpairix")
+
+  # function
+  x1 = px_query(filename,"chrX|chrX", linecount.only=TRUE)
+
+  # benchmark
+  command = paste("gunzip -c ",filename," | awk '$2==\"chrX\" && $4==\"chrX\"' |wc -l", sep="")
+  x2=system(command, intern=TRUE)
   x2 = as.integer(x2)
+
+  # compare
   expect_equal(x1, x2)
 })
 
